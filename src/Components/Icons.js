eae7a03d054cd10eac6578d4669c2696bc1e2179
eunheexcho <vialactea128@gmail.com>
2020-03-10 17:34:08 +0900

    Feed/Loader Component

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -60,11 +60,11 @@ export const Comment = () => (
     </svg>
 );
 
-export const Logo = () => (
+export const Logo = ({size = 24}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
+        width={size}
+        height={size}
         viewBox="0 0 24 24"
     >
         <path
